48ae88a1ef589897fdbf81398ad39bee4e34e83a
githubPrabhsingh <101601501+githubPrabhsingh@users.noreply.github.com>
2023-06-15 09:46:59 +0530

    Add files via upload

---
+++ Frontend/resume-builder/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import ResumeSectionView from "./components/ResumeSectionView";
+import "./components/ResumeSectionView.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeSectionView />
+    </div>
+  );
+}
+
+export default App;
